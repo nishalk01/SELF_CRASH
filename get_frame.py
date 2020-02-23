3c5f29246939bcffd4da504c71be783046398f2f
nishalk01 <45785279+nishalk01@users.noreply.github.com>
2020-02-23 08:03:57 -0800

    Update get_frame.py

--- get_frame.py
+++ get_frame.py
@@ -3,7 +3,7 @@ from PIL import ImageGrab
 import cv2
 
 def get_frames():
-    im = ImageGrab.grab((60,60,600,600))
+    im = ImageGrab.grab((0,40,800,640))
     im=np.array(im)
     im=cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
     im=cv2.resize(im,(80,60))
